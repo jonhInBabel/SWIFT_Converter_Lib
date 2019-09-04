55e9329897a0a7ccf4faccc3052f7aac8b32c6cd
jose.galan <jose.galan@babel.es>
2019-09-04 08:52:50 +0200

    Añadidas 3 clases de reglas para MT900 mapper

---
+++ src/main/java/com/babel/swift/rules/MT900/MTRules/AddressLinesFromField52D.java
@@ -0,0 +1,51 @@
+package com.babel.swift.rules.MT900.MTRules;
+
+import com.babel.swift.exceptions.MTFieldParsingException;
+import com.babel.swift.rules.IMTRule;
+import com.prowidesoftware.swift.model.field.Field52D;
+import iso.std.iso._20022.tech.xsd.camt_054_001_02.PostalAddress6;
+import org.springframework.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddressLinesFromField52D implements IMTRule {
+
+    /*
+	public static List<String> getField20Value( Field52D field52D ) {
+		List<String> nameAndAddress_lines = new ArrayList<String>();
+		if(field52D != null) {
+			nameAndAddress_lines.add(field52D.getNameAndAddressLine1());
+			nameAndAddress_lines.add(field52D.getNameAndAddressLine2());
+			nameAndAddress_lines.add(field52D.getNameAndAddressLine3());
+			nameAndAddress_lines.add(field52D.getNameAndAddressLine4());
+		}
+
+		return nameAndAddress_lines;
+	}
+	*/
+
+    @Override
+    public Object apply(Object mtField) throws MTFieldParsingException {
+        Field52D field52D = (Field52D) mtField;
+
+        //Ntfctn
+        List<String> adrLine = null;
+        PostalAddress6 pstlAdr = null;
+
+        if( !StringUtils.isEmpty( field52D ) ) {
+            adrLine = new ArrayList<String>();
+            adrLine.add(field52D.getNameAndAddressLine2());
+            adrLine.add(field52D.getNameAndAddressLine3());
+            adrLine.add(field52D.getNameAndAddressLine4());
+            if(!adrLine.isEmpty()) {
+                pstlAdr = new PostalAddress6();
+                pstlAdr.getAdrLine().addAll(adrLine);
+            }
+
+        }
+
+        return pstlAdr;
+    }
+
+}
