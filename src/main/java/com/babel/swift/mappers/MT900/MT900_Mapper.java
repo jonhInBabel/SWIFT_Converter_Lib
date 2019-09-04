681e5bf3f939697772787ac6fb9cbc8901463973
jose.galan <jose.galan@babel.es>
2019-09-04 09:14:22 +0200

    Corrección de errores en los tipos devueltos por las reglas

--- src/main/java/com/babel/swift/mappers/MT900/MT900_Mapper.java
+++ src/main/java/com/babel/swift/mappers/MT900/MT900_Mapper.java
@@ -7,10 +7,7 @@ import com.babel.swift.rules.MT900.MTRules.*;
 import com.prowidesoftware.swift.model.mt.AbstractMT;
 import com.prowidesoftware.swift.model.mt.mt9xx.MT900;
 import com.prowidesoftware.swift.model.mx.AbstractMX;
-import iso.std.iso._20022.tech.xsd.camt_054_001_02.ActiveOrHistoricCurrencyAndAmount;
-import iso.std.iso._20022.tech.xsd.camt_054_001_02.DateAndDateTimeChoice;
-import iso.std.iso._20022.tech.xsd.camt_054_001_02.Document;
-import iso.std.iso._20022.tech.xsd.camt_054_001_02.PostalAddress6;
+import iso.std.iso._20022.tech.xsd.camt_054_001_02.*;
 
 public class MT900_Mapper extends AMapper {
 
@@ -21,18 +18,17 @@ public class MT900_Mapper extends AMapper {
 	@Override
 	protected void applyMTRules(AbstractMT mtMessage) throws MTFieldParsingException {
 		MT900 mt900 = (MT900) mtMessage;
-		this.mxObject = new Document();
 		Document document = new Document();
-		
+
 		/*
-			aqui componemos el objeto root de la clase obtenida a partir del xsd usando las reglas específicas
+		 * aqui componemos el objeto root de la clase obtenida a partir del xsd usando las reglas específicas
 		 */
 
-		BranchAndFinancialInstId4FromLTA branchAndFinancialInstId4FromLTA 		= (BranchAndFinancialInstId4FromLTA) new BranchAndFinancialInstId4FromLTA().apply( mt900.getLogicalTerminal() );
-		PartyIdentification32FromIDA partyIdentification32FromIDA 				= (PartyIdentification32FromIDA) new PartyIdentification32FromIDA().apply( mt900.getReceiver() );
+		FinancialInstitutionIdentification7 financialInstitutionIdentification7 = (FinancialInstitutionIdentification7) new BranchAndFinancialInstId4FromLTA().apply( mt900.getLogicalTerminal() );
+		PartyIdentification32 partyIdentification32 							= (PartyIdentification32) new PartyIdentification32FromIDA().apply( mt900.getReceiver() );
 		DateAndDateTimeChoice dateAndDateTimeChoice 							= (DateAndDateTimeChoice) new DateFromField32A().apply( mt900.getField32A() );
 		ActiveOrHistoricCurrencyAndAmount activeOrHistoricCurrencyAndAmount  	= (ActiveOrHistoricCurrencyAndAmount) new CurrencyAndAmountFromField32A().apply(mt900.getField32A());
-		String nameAndAddress													= (String) new NameFromField52D().apply( mt900.getField52D() );
+		String name																= (String) new NameFromField52D().apply( mt900.getField52D() );
 		PostalAddress6 postalAddress6 											= (PostalAddress6) new AddressLinesFromField52D().apply( mt900.getField72() );
 		String additionalTransactionInfo 										= (String) new AdditionalTransactionInfFromField72().apply( mt900.getField72() );
 
